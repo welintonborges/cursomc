976475a9696716c4201efbcb565083a644741860
Welinton Borges <Welinton.b.borges@gmail.com>
2023-03-19 11:40:10 -0300

    criando o salvar de categoraias

--- src/main/java/com/example/testeando/repositories/PessoaRepository.java
+++ src/main/java/com/example/testeando/repositories/PessoaRepository.java
@@ -4,8 +4,10 @@ import com.example.testeando.domain.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+    Optional<Pessoa> findById(Integer id);
 }
