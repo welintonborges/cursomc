18843716d91e830d2e20231d9b98c125497cd48a
Welinton Borges <Welinton.b.borges@gmail.com>
2023-04-06 07:22:55 -0300

    criação das classes Pedido, Pagamento, PagamentoComBoleto, PagamentoComCartao e EstadoPagamento.

---
+++ src/main/java/com/example/testeando/domain/PagamentoComCartao.java
@@ -0,0 +1,24 @@
+package com.example.testeando.domain;
+
+import com.example.testeando.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends  Pagamento{
+    private static final long serialVersionUID = 1L;
+
+    private Integer numeroDeParcelas;
+
+    public  PagamentoComCartao(){}
+
+    public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+        super(id, estado, pedido);
+        this.numeroDeParcelas = numeroDeParcelas;
+    }
+
+    public Integer getNumeroDeParcelas() {
+        return numeroDeParcelas;
+    }
+
+    public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+        this.numeroDeParcelas = numeroDeParcelas;
+    }
+}
