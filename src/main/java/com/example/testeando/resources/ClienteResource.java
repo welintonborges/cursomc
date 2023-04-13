ac1504ff06eefdca3bfed4623fd612820a0d7430
Welinton Borges <Welinton.b.borges@gmail.com>
2023-04-13 15:33:19 -0300

    Atualizando categoria com o PUT e outros pequenos ajustes.

--- src/main/java/com/example/testeando/resources/ClienteResource.java
+++ src/main/java/com/example/testeando/resources/ClienteResource.java
@@ -16,7 +16,7 @@ public class ClienteResource {
     private ClienteService service;
     @RequestMapping(value = "{id}",method = RequestMethod.GET)
     public ResponseEntity<?> listar(@PathVariable Integer id){
-        Cliente obj = service.buscar(id);
+        Cliente obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
 }
