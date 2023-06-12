01ffc1ce6c431cd6eec85a1a1ea51c766797b6f2
Welinton Borges <Welinton.b.borges@gmail.com>
2023-06-12 15:36:09 -0300

    Criando profile teste

--- src/main/java/com/example/testeando/TesteandoApplication.java
+++ src/main/java/com/example/testeando/TesteandoApplication.java
@@ -1,16 +1,9 @@
 package com.example.testeando;
 
-import com.example.testeando.domain.*;
-import com.example.testeando.domain.enums.EstadoPagamento;
-import com.example.testeando.domain.enums.TipoCliente;
-import com.example.testeando.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 
 @SpringBootApplication
